74ef765f7ec8f02098b682172b00d4f7cdf52358
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-11-02 10:10:18 +0100

    Dashboard redirect

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Registries - Dashboard
+ *
+ * @package Coordinator\Modules\Registries
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ // redirect to registries list
+ api_redirect(api_url(["scr"=>"registries_list"]));
+?>
