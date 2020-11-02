983750cc175b4615fa89f374c91f1fff6a98f78e
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-11-02 16:34:16 +0100

    Module informations

---
+++ module.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Registries
+ *
+ * @package Coordinator\Modules\Registries
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ // definitions
+ $module_name="registries";
+ $module_repository_url="https://github.com/Zavy86/Coordinator-Registries/";
+ $module_repository_version_url="https://raw.githubusercontent.com/Zavy86/Coordinator-Registries/master/VERSION.txt";
+ $module_required_modules=array("archive");
+?>
